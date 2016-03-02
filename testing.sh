ba19689d19b84f192cef055807276baa592ea7d0
Morteza Ramezani <morteza.r25@gmail.com>
2016-03-01 21:50:58 -0500

    added testing, running, and grader

--- testing.sh
+++ testing.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
 CODE_PATH=$1
-INPUT_FILE1="../../scripts/input1"
-OUTPUT_NAME="output.txt"
-TIMEF_NAME="time.txt"
+OUTPUT_CSV="status.csv"
+
+echo "stdid, makefile, bashscript, readme" > $OUTPUT_CSV
 
 for std_path in ${CODE_PATH}/*/
 do
+  std_has_make=false
+  std_has_bash=false
+  std_has_readme=false
+
   std_temp=$(basename "$std_path")
   std_name=$(echo $std_temp | tr "()" "\n" | sed -n 2p)
-  echo $std_name >&2
-  #echo $std_path
+
+  echo $std_name
+  echo -n $std_name", " >> $OUTPUT_CSV
+
   cd "$std_path"
+
   make > /dev/null 2>&1
   ret_val=$?
+
   if [ $ret_val != 0 ]; then
     echo "ERROR"
+    echo -e 0", " >> $OUTPUT_CSV
   else
     echo "OK"
-    ./wordc $INPUT_FILE1 $OUTPUT_NAME $TIMEF_NAME
-    ./wordc.sh $INPUT_FILE1 $OUTPUT_NAME_SH $TIMEF_NAME_SH
-    python $GRADER
-    head -n 5 $OUTPUT_NAME
-    cat $TIMEF_NAME
+    echo -e 1", " >> $OUTPUT_CSV
   fi
+
   echo '----------------------------------------'
   cd ..
 done
