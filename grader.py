a5b30d16eb2f4505c00141a06bdcb2a76cb6357e
Morteza Ramezani <morteza.r25@gmail.com>
2016-03-02 00:28:34 -0500

    starting grader

--- grader.py
+++ grader.py
@@ -1,3 +1,28 @@
 from __future__ import print_function
 
-print("100")
+import os
+import sys
+
+if __name__ == "__main__":
+
+	if len(sys.argv) < 4 :
+		sys.exit('Usage: ...')
+	else:
+		input_file  = sys.argv[1]
+		code_output = sys.argv[2]
+		bash_output = sys.argv[3]
+
+	score = 0
+
+	if not os.path.isfile(code_output) and not os.path.isfile(bash_output) :
+		print(score)	
+		sys.exit(1) 
+	
+	if os.stat(code_output).st_size==0 and os.stat(bash_output).st_size==0 :
+		print(score)	
+		sys.exit(1)
+	
+	with 
+
+	score=10
+	print(score)
